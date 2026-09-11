8492d5b6b677ec7bba6963b382dc052390559fdb
Trần Phước Minh Tùng <tranminhtung2007@gmail.com>
2026-09-11 22:15:53 +0700

    Add documentation comment for Express class

--- Week3/TrainSchedule/src/trainschedule/Express.java
+++ Week3/TrainSchedule/src/trainschedule/Express.java
@@ -1,6 +1,6 @@
 package trainschedule;
 
-/** An express train. */
+
 public class Express extends KindOfTrain {
     public Express(double price, int carriageNumber) {
         super(price, carriageNumber);
